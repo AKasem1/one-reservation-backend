71dd92ccce5ed26a57c440a9a15ec861ff4cec38
Abdelrahman Kasem <abdelrahmankasem7@gmail.com>
2023-08-02 08:06:49 +0300

    one

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -7,6 +7,7 @@ const createToken = (_id) => {
   }
 
 const loginAdmin = async (req, res) => {
+    console.log("here received request..s")
     const {username, password} = req.body
     console.log({username, password})
     try{
